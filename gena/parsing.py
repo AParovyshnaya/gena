35b306294fb14aa84f2da905bce63c3f691eaddd
Anastasiia Parovyshnaia <anastasiia.parovyshnaia@arsysop.ru>
2022-12-27 21:11:19 +0300

    css продвигается

--- gena/parsing.py
+++ gena/parsing.py
@@ -3,7 +3,6 @@ import os
 import shutil
 def enter():
     path = input("Абсолютный путь к папке с data.json и папкой images, содержащей папки ad и tech в корне : ").replace('\\', '/')
-    print(path)
     path += "/"
     return path
 def createSite():
@@ -15,44 +14,47 @@ def createSite():
     os.mkdir(path + "site/images/site")
     os.mkdir(path + "site/pages")
     os.mkdir(path + "site/pages/commodites")
+    os.mkdir(path + "site/pages/commodites/css")
 def createCommodites():
     with open(path + 'data.json', encoding="UTF-8") as file:
         data = json.load(file)        
         commodities = data.get('commodity')
         return commodities
-def createFile(adress, write='', old_adress='', our=False):
+def createFile(adress, div='', old_adress='', our=False):
     new_path = path + adress
     file = open(new_path, "w", encoding="UTF-8")
     if our:
         old_path = os.getcwd().replace("\\", "/") + "/" + old_adress
         file = open(new_path, "w", encoding="UTF-8")
         shutil.copyfile(old_path, new_path)
-    file.write(write)
+    file.write(div)
     return file
 def genetateSite():
-    index = createFile("site/index.html", "<!DOCTYPE html><html><head><title>gena</title><meta charset='utf-8'><meta name='viewport' content='width=device-width', initial-scale=1, shrink-to-fit='no'><link rel='stylesheet' href='css/main.css'></link><link rel='stylesheet' href='css/bootstrap.min.css'></link></head><body><div class='header'><div>Это Gena</div><div><img src='images/site/icon.png'></div></div><div id='body'>")
-    css = createFile("site/css/main.css", old_adress="files/css/main.css", our = True)
-    bootsrap = createFile("site/css/bootstrap.min.css", old_adress="files/css/bootstrap.min.css", our = True)
+    index = createFile("site/index.html", "<!DOCTYPE html><html><head><title>gena</title><meta charset='utf-8'><meta name='viewport' content='width=device-width', initial-scale=1, shrink-to-fit='no'><link rel='stylesheet' href='css/main.css'></link><link rel='stylesheet' href='css/bootstrap.min.css'></link></head><body class='body'><div class='header container text-center'><div class='row justify-content-md-center'><div class='col-md-auto'><a class='a' href='index.html'>Это Gena</a></div><div class='col-md-auto'><a class='a' href='index.html'><img src='images/site/icon.png'></a></div></div></a></div><div class='body container'><div class='row'>")
+    createFile("site/css/main.css", old_adress = "files/css/main.css", our = True)
+    createFile("site/css/bootstrap.min.css", old_adress="files/css/bootstrap.min.css", our = True)
     copyImage("images/site/phone.jpg", "phone", "phone", "site")
     copyImage("images/site/icon_64.png", "icon", "icon", "site")
     for commodity in createCommodites():
         id = commodity.get("id")
-        html = createFile("site/pages/commodites/" + id[3:] + ".html", f"<!DOCTYPE html><html><head><title>{commodity.get('name')}</title><link rel='stylesheet' href='css/main.css'><meta charset='utf-8'></link><link rel='stylesheet' href='css/bootstrap.min.css'></link></head><body>")
-        html.write(f"<h1>{commodity.get('name')}</h1>")
-        html.write(f"<p>{commodity.get('article')}</p>")
+        createFile("site/pages/commodites/css/main.css", old_adress="files/css/main.css", our = True)
+        createFile("site/pages/commodites/css/bootstrap.min.css", old_adress="files/css/bootstrap.min.css", our = True)
+        html = createFile("site/pages/commodites/" + id[3:] + ".html", f"<!DOCTYPE html><html><head><title>{commodity.get('name')}</title><link rel='stylesheet' href='css/main.css'><meta charset='utf-8'></link><link rel='stylesheet' href='css/bootstrap.min.css'></link></head><body class='commodites body'><div class='header container text-center'><div class='row justify-content-md-center'><div class='col-md-auto'><a class='a' href='../../index.html'>Это Gena</a></div><div class='col-md-auto'><a class='a' href='../../index.html'><img src='../../images/site/icon.png'></a></div></div></a></div><div class='body container'>")
+        html.write(div(commodity.get('name'), center=True))
+        html.write(div(commodity.get('article')))
         price = commodity.get('price')
-        html.write(f"<b>{price.get('value')}</b>")
+        html.write(div(price.get('value')))
         product = commodity.get('product')
-        html.write(f"<p>Вес: {product.get('weight')} г</p>")
+        html.write(div(product.get('weight')))
         if product.get('reserve') == 0:
-            html.write("<p>Данного товара на складе нет</p>")
+            html.write(div("Данного товара на складе нет"))
         else:
-            html.write("<p>На складе есть этот товар</p>")
+            html.write(div("На складе есть этот товар"))
         story = commodity.get('adverting_story')    
-        html.write(f"<p>{story.get('text')}</p>")
+        html.write(div(story.get('text')))
         image(story, 'adveting_photo', html)
-        index.write(f"<div><a href={'pages/commodites/' + id[3:] + '.html'}><p><img src='{image(product, 'technical_photo', html)}' style='width: 300px'>{commodity.get('name')}</p></a></div>")      
-        html.write("</body></html>")
+        index.write(f"<div class='col-6'><a class='a' href={'pages/commodites/' + id[3:] + '.html'}><div class='col-4'><img src='{image(product, 'technical_photo', html)}' style='width: 300px'></div><div class='col-2'>{commodity.get('name')}</div></a></div>")      
+        html.write("</div></body></html>")
     index.write("</div></body></html>")
 def copyImage(src, id, alt, directory, html = '', cut = False):
     old_path = path + src
@@ -69,7 +71,7 @@ def copyImage(src, id, alt, directory, html = '', cut = False):
                 name = id
             new_path = "site/images/" + directory + "/" + name + exstension
             if html != '':
-                html.write(f"<img src='{'../../' + new_path[5:]}' alt='{alt}' style='width: 700px'>")
+                html.write(div(f"<img class='img-fluid' src='{'../../' + new_path[5:]}' alt='{alt}'>"))
             new_path = path + new_path
             site_image = open(new_path, "w")
             shutil.copyfile(old_path, new_path)
@@ -87,7 +89,11 @@ def image(object, name, html):
             ad_or_tech = "ad"
         for image in images:
             return copyImage(image.get("src"), image.get("id"), image.get("name"), ad_or_tech, html, True)
-        
+def div(content, center = False):
+    if center:
+        return f"<div class='container text-center'><div class='row justify-content-md-center'><div class='col'>{content}</div></div></div>"        
+    else:
+        return f"<div class='container'><div class='row'><div class='col'>{content}</div></div></div>"
 path = enter()
 #В виджетах
 # темы - новогодняя и обычнаяE:/work/gena/git/gena/data
