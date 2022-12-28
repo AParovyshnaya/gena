91972777b419d3cf81d589d9f0d28e8e2d6493ad
Anastasiia Parovyshnaia <anastasiia.parovyshnaia@arsysop.ru>
2022-12-28 12:12:48 +0300

    hello classes!

---
+++ gena/src/Gena.py
@@ -0,0 +1,9 @@
+class Gena():
+    
+    def __init__(self, input, output):
+        self.input = input
+        self.output = output
+    
+    def generate(self):
+        print(f"from {self.input} to {self.output}")
+        
---
+++ gena/src/main.py
@@ -0,0 +1,3 @@
+from Gena import Gena
+
+Gena('a', 'b').generate()
