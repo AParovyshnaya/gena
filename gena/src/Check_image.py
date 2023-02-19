4d81344357e20c76d4e5dddac878d8ff81153529
user <user@DESKTOP-N44SLJM>
2023-02-19 17:15:49 +0300

    собираем exe

--- gena/src/Check_image.py
+++ gena/src/Check_image.py
@@ -1,10 +1,12 @@
 import sys
+import os
+
 class Image():
     def __init__(self):
         pass
     
     def check(self):
         if getattr(sys, 'frozen', False):
-            return(os.path.join(sys._MEIPASS, "icon.ico"))
+            return(os.path.join(sys._MEIPASS, "icons/icon.ico"))
         else:
-            return("icon.ico")
+            return("icons/icon.ico")
